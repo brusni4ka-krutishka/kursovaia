edc351dfdcdaea1d872273a64aed7ff2aa8665a8
EUGENE <evgenijgoncarevic@gmail.com>
2021-12-05 14:02:29 +0300

    fad

---
+++ script.js
@@ -0,0 +1,4 @@
+// window.addEventListener("scroll", function () {
+//   document.getElementsByTagName("header")[0].style.position =
+//     window.pageYOffset === 190 ? "relative" : "fixed";
+// });
