0aa1fefc8dd8dd67f5638914411d16d9104141e2
EUGENE <evgenijgoncarevic@gmail.com>
2021-12-21 19:49:06 +0300

    menu fixed

--- js/header.js
+++ js/header.js
@@ -1,6 +1,7 @@
 document.getElementById('head').innerHTML = `<meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <link rel="stylesheet" href="./css/styles.css" />
+    <link rel="shortcut icon" href="./img/icon.jpg" type="image/x-icon" />
     <link rel="stylesheet" href="./css/reset.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Коктейль-бар</title>`;
