f5488616c2596e4d72915b280084a5d27314dda5
EUGENE <evgenijgoncarevic@gmail.com>
2021-12-23 15:06:46 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -102,7 +102,7 @@ function onEntry(entry) {
 }
 
 let options = {
-  threshold: [0.5],
+  threshold: [0.3],
 };
 let observer = new IntersectionObserver(onEntry, options);
 let elements = document.querySelectorAll('.human, .portfolio');
